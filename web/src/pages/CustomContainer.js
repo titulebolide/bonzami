86b69c8fd87c9e1679b6eca1eb6571cd6416ffa3
titulebolide <seneztit@gmail.com>
2024-04-07 23:40:29 +0200

    improve expense viewer on mobile

--- web/src/pages/CustomContainer.js
+++ web/src/pages/CustomContainer.js
@@ -2,10 +2,8 @@ import { Outlet } from "react-router-dom"
 
 export default function CustomContainer() {
   return (
-    <section className="section">
-      <div className="container" style={{maxWidth:600}}>
-        <Outlet />
-      </div>
-    </section>
+    <div className="container" style={{maxWidth:600}}>
+      <Outlet />
+    </div>
   )
 }
--- web/src/pages/Expense.js
+++ web/src/pages/Expense.js
@@ -1,17 +1,39 @@
-import { useLoaderData } from "react-router-dom";
-
+import { useLoaderData, useParams } from "react-router-dom";
+import "./Expense.css"
 
 export async function expenseLoader({params}) {
-  console.log(params)
   const res = await fetch("http://127.0.0.1:8000/api/g/" + params.guid + "/e/" + params.expenseid + "/info")
-  return res.json()
+  let dat = res.json()
+  dat.guid = params.guid
+  dat.expenseid = params.expenseid
+  return dat
+}
+
+function ToolHeader({leftIcon, leftIconOnClick, rightIcon, rightIconOnClick}) {
+  return (
+    <div id="tool-hdr">
+      <div className="tool-hdr-btn" onClick={leftIconOnClick}>
+        <i className={leftIcon}></i>
+      </div>
+      <div className="tool-hdr-btn" onClick={rightIconOnClick}>
+        <i className={rightIcon}></i>
+      </div>
+    </div>  
+  )
 }
 
 export default function Expense() {
   const expense = useLoaderData();
-  console.log(expense)
+  let params = useParams();
+  console.log(params)
   return (
     <>
+      <ToolHeader
+        leftIcon = "ri-arrow-left-line"
+        leftIconOnClick = {() => {window.location = `/g/${params.guid}`}}
+        rightIcon = "ri-pencil-fill"
+        rightIconOnClick = {() => {}}
+      />
       <div className="box">
         <div className="columns">
           <div className="column">
@@ -25,15 +47,14 @@ export default function Expense() {
       </div>
       <div className="section">
         <div className="title is-5">Shares</div>
-        <div className="columns">
+        <div className="columns is-mobile">
           <div className="column is-offset-1">
             {
               expense.shares.map((share, index) => (
-                <div className="columns" key={index}>
+                <div className="columns is-mobile" key={index}>
                   <div className="column" style={{textAlign: "right"}}>{share.uname}</div>
                   <div className="column">{share.share}</div>
                 </div>
-
               ))
             }
           </div>
