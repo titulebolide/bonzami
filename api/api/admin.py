9d490fa1aecdf0b1eef1c5fdc9e7896f5ceb92d8
titulebolide <seneztit@gmail.com>
2025-12-29 10:40:20 +0100

    improve admin view

--- api/api/admin.py
+++ api/api/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.http import urlencode
 
 from . import models
 
+class UserInline(admin.TabularInline):
+    model = models.User
+    extra = 0
+
+class CategoryInline(admin.TabularInline):
+    model = models.Category
+    extra = 0
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [UserInline, CategoryInline]
+    readonly_fields = ["view_all_expenses"]
+
+    def view_all_expenses(self, obj):
+        count = obj.expense_set.count()
+        url = (
+            reverse("admin:api_expense_changelist")
+            + "?"
+            + urlencode({"group__id": f"{obj.id}"})
+        )
+        return format_html(
+            '<a href="{}">View all {} expenses (showing latest 50 inlines)</a>',
+            url,
+            count,
+        )
+
+    view_all_expenses.short_description = "Expenses"
+
 admin.site.register(models.User)
-admin.site.register(models.Group)
+admin.site.register(models.Group, GroupAdmin)
 admin.site.register(models.Expense)
 admin.site.register(models.ExpenseShare)
 admin.site.register(models.Category)
--- api/api/models.py
+++ api/api/models.py
@@ -5,14 +5,23 @@ class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name} - {self.id}" 
+
 class User(models.Model):
     name = models.CharField(max_length=50)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.id}" 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     emoji = models.CharField(max_length=8) # stored as AARRGGBB
+
+    def __str__(self):
+        return f"{self.emoji} {self.name} - {self.id}"
 
 class Expense(models.Model):
     name = models.CharField(max_length=50)
@@ -22,7 +31,13 @@ class Expense(models.Model):
     by = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return f"{self.name} - {self.id}" 
+
 class ExpenseShare(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
     share = models.FloatField()
+
+    def __str__(self):
+        return f"{self.user.name} - {self.expense.name} - {self.share}"
